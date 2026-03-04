34730b88b31e0c42f6589c708f7dbf7e2d52b258
Evgenii <filatov.j95@gmail.com>
2026-03-04 14:55:22 +0300

    Написал тест test_registration_random_country

---
+++ tests/RegistrationTests.py
@@ -0,0 +1,18 @@
+import allure
+from core.BaseTest import browser
+from pages.BasePage import BasePage
+from pages.LoginPage import LoginPageHelper
+from pages.RegistrationPage import RegistrationPageHelper
+
+BASE_URL = 'https://ok.ru/'
+
+@allure.suite('Проверка выпадающего списка')
+@allure.title('Код страны в поле "Номер телефона" соответствует выбранной из списка')
+def test_registration_random_country(browser):
+    BasePage(browser).get_url(BASE_URL)
+    login_page = LoginPageHelper(browser)
+    login_page.click_registration()
+    registration_page = RegistrationPageHelper(browser)
+    selected_country_code = registration_page.select_random_country()
+    actual_country_code = registration_page.get_phone_field_value()
+    assert selected_country_code == actual_country_code
