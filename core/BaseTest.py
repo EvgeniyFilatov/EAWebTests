d41e446c2dfe15b05d538fab941e6e2f7231e092
Evgenii <filatov.j95@gmail.com>
2026-03-27 12:38:53 +0300

    Внес изменения для запуска в Jenkins

--- core/BaseTest.py
+++ core/BaseTest.py
@@ -6,7 +6,9 @@ from selenium import webdriver
 def browser():
     options = webdriver.ChromeOptions()
     options.add_argument("--lang=ru")
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Remote(command_executor="http://85.198.69.66:4444", options=options)
     yield driver
-    if driver:
-        driver.quit()
+    driver.quit()
